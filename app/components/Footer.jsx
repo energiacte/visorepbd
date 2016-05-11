7af2db2fa1ee3e68db48cae32330a9632bbdac73
Rafael Villar Burke <pachi@ietcc.csic.es>
2016-05-11 12:10:49 +0200

    Footer: nuevo componente de pie de página

---
+++ app/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default React.createClass({
+
+  render() {
+    return (
+      <div className="navbar navbar-bottom text-center" style={{ marginTop: 20 + 'px' }}>
+        <p className="text-muted small center">EPBDPanel - 2016 - <a href="http://www.fomento.gob.es">Ministerio de Fomento</a>, <a href="http://www.ietcc.csic.es">Instituto Eduardo Torroja de Ciencias de la Construcción (IETcc-CSIC)</a>.</p>
+      </div>
+    );
+  }
+});
