f98557c65b294a9e13194a61763004895d19b424
Rafael Villar Burke <pachi@ietcc.csic.es>
2016-06-28 17:18:29 +0200

    Añadir acción que faltaba para EnergyComponentList

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -14,6 +14,7 @@ import { changeKexp,
          addEnergyComponent,
          removeEnergyComponent,
          editEnergyComponent,
+         selectEnergyComponent,
          fetchData } from 'actions/actions.js';
 
 class App extends React.Component {
