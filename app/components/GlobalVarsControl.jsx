8d5ebf5f9074f2b6b21cd1304a1f9c214fcba224
Rafael Villar Burke <pachi@ietcc.csic.es>
2016-06-29 12:53:58 +0200

    GlobalVarsControl: mejorar estilo

--- app/components/GlobalVarsControl.jsx
+++ app/components/GlobalVarsControl.jsx
@@ -9,35 +9,32 @@ export default class GlobalVarsControl extends React.Component {
     const { kexp, krdel, area, onChangeKexp, onChangeKrdel, onChangeArea } = this.props;
 
     return (
-      <div>
-        <div id="kexp">
-          <span>k<sub>exp</sub> </span>
+      <div className="form-horizontal bg-primary">
+        <div id="kexp" className="form-group">
+          <label className="control-label" htmlFor="kexprange">k<sub>exp</sub> </label>
           <input type="range" min="0" max="1" step="0.1"
+                 name="kexprange"
                  className="form-control"
                  defaultValue={ kexp }
                  onChange={ e => onChangeKexp(e.target.value) } />
-          <span>&nbsp;&nbsp;</span>
           <input type="text" readOnly maxLength="3" size="3"
                  value={ numeral(kexp).format('0.0') } />
         </div>
-        <div id="krdel">
-          <span>k<sub>rdel</sub> </span>
+        <div id="krdel" className="form-group">
+          <label className="control-label" htmlFor="krdelrange">k<sub>rdel</sub> </label>
           <input type="range" min="0" max="1" step="0.1"
+                 name="krdelrange"
                  className="form-control"
                  defaultValue={ krdel }
                  onChange={ e => onChangeKrdel(e.target.value) } />
-          <span>&nbsp;&nbsp;</span>
           <input type="text" readOnly maxLength="3" size="3"
                  value={ numeral(krdel).format('0.0') } />
         </div>
-        <div id="area" className="pull-right">
-          <span>Superficie de referencia (m²) </span>
-          <input ref={ (ref) => this.areaEntry = ref }
+        <div id="area" className="form-group col-md-3 pull-right">
+          <label className="control-label" htmlFor="areaentry">Superficie (m²) </label>
+          <input type="number" lang="es" min="1" step="1"
                  name="areaentry"
-                 type="number"
-                 lang="es"
-                 min="1"
-                 step="1"
+                 className="form-control"
                  defaultValue={ numeral(area).format('0') }
                  onChange={ e => onChangeArea(e.target.value) } />
         </div>
