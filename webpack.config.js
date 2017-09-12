46be5ffe3d8adf3400be339e4e7bd54cfadf560b
Rafael Villar Burke <pachi@ietcc.csic.es>
2017-09-12 15:04:21 +0200

    Webpack: aplica algunos trucos de create-react-app

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 'use strict';
 
-const path = require('path');
 const autoprefixer = require('autoprefixer');
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin'); 
 const CleanPlugin = require('clean-webpack-plugin');
 
 const production = process.env.NODE_ENV === 'production';
+// Source maps are resource heavy and can cause out of memory issue for large source files.
+const shouldUseSourceMap = production ? process.env.GENERATE_SOURCEMAP !== 'false' : true;
+
+
 // Permitirmos usar la variable de entorno EPBDURLPREFIX para añadir prefijo
 // a las direcciones estáticas y la url del servicio que llamamos con ajax
 const epbdurlprefix = process.env.EPBDURLPREFIX || '';
@@ -39,7 +44,15 @@ var plugins = [
     inject: false,
     //favicon: 'favicon.ico',
     filename: './index.html', // relativo al output path
-    minify: { removeComments: true, collapseWhitespace: true }
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      keepClosingSlash: true
+    }
   }),
   new webpack.NoEmitOnErrorsPlugin(),
   new webpack.optimize.CommonsChunkPlugin({
@@ -57,9 +70,11 @@ if (production) { // Production plugins go here
     // Prevent Webpack from creating too small chunks
     new webpack.optimize.MinChunkSizePlugin({ minChunkSize: 51200 }), // ~50kb
     // Minify all the Javascript code of the final bundle
-    new webpack.optimize.UglifyJsPlugin({ minimize: true,
-                                          sourceMap: true,
-                                          compress: { warnings: false } }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: shouldUseSourceMap,
+      compress: { warnings: false }
+    }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
@@ -80,7 +95,8 @@ if (production) { // Production plugins go here
 
 var config = {
   cache: true,
-  devtool: production ? 'cheap-module-source-map': 'cheap-module-eval-source-map',
+  //devtool: production ? 'cheap-module-source-map': 'cheap-module-eval-source-map',
+  devtool: production ? (shouldUseSourceMap ? 'cheap-module-source-map' : false) : 'cheap-module-eval-source-map',
   entry: {
     app: [
       'babel-polyfill',
@@ -90,14 +106,15 @@ var config = {
   },
   output: {
     path: PATHS.build,
-    filename: '[name]-[hash].js',
+    pathinfo: true, // /* filename */ comments to generated requires in otuput
+    filename: '[name]-[hash:8].js',
     // publicPath: "http://localhost:8080/", // Development server
     // publicPath: "http://example.com/", // Production
     publicPath: production ? epbdurlprefix : '' // This is used to generate URLs to e.g. images,css
   },
   resolve: {
     modules: [PATHS.app, PATHS.node, PATHS.nodedir],
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.web.js', '.js', '.web.jsx', '.jsx', '.json'],
     alias: { // Para usar alias en imports
       'styles': PATHS.styles,
       'components': PATHS.components,
@@ -109,13 +126,31 @@ var config = {
     }
   },
   module: {
+    strictExportPresence: true,
     rules: [
+      // First, run the linter.
+      // It's important to do this before Babel processes the JS.
+      {
+        test: /\.(js|jsx)$/,
+        include: PATHS.app,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              formatter: eslintFormatter,
+              eslintPath: require.resolve('eslint'),
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+      },
       { // JS, JSX: BABEL
         test: /\.jsx?$/,
         include: PATHS.app,
         loader: 'babel-loader',
         options: {
-          cacheDirectory: true,
+          cacheDirectory: production ? false : true,
+          compact: production ? true : false,
           presets: [['es2015', { modules: false }], 'stage-0', 'react', 'flow']
         }
       },
@@ -125,10 +160,25 @@ var config = {
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
-            { loader: 'css-loader' },
+            { loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: production ? true: false,
+                sourceMap: production ? shouldUseSourceMap : false
+              }
+            },
             { loader: 'postcss-loader',
               options: {
-                plugins: function () { return [autoprefixer('last 2 versions', 'ie 10')] }
+                plugins: () => [
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ]
+                  })
+                ]
               }
             }
           ]
@@ -143,7 +193,16 @@ var config = {
             { loader: 'css-loader' },
             { loader: 'postcss-loader',
               options: {
-                plugins: function () { return [autoprefixer('last 2 versions', 'ie 10')] }
+                plugins: () => [
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ]
+                  })
+                ]
               }
             },
             { loader: 'sass-loader', options: { outputStyle: 'expanded' } }
@@ -151,27 +210,43 @@ var config = {
         })
       },
       { // IMG  direct URLs for the rest
-        test: /\.(png|jpe?g|gif)$/i,
+        test: [/\.bmp$/i, /\.gif$/i, /\.jpe?g$/i, /\.png$/i],
         include: PATHS.app,
-        loader: ['img-loader', 'file-loader?name=img/[name]-[hash].[ext]']
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'img/[name]-[hash:8].[ext]'
+            }
+          },
+          'img-loader'
+        ]
       },
       { // .ico files
         test: /\.ico$/i,
         include: PATHS.app,
-        loader: ['img-loader', 'file-loader?name=[name].[ext]']
+        loader: ['file-loader?name=[name].[ext]', 'img-loader']
       },
       // required for bootstrap icons
-      { test: /\.woff2?$/,                     loader: 'file-loader?name=fonts/[name]-[hash].[ext]' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: 'file-loader?name=fonts/[name]-[hash].[ext]' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: 'file-loader?name=fonts/[name]-[hash].[ext]' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: 'file-loader?name=img/[name]-[hash].[ext]' },
+      { test: /\.woff2?$/,
+        loader: 'file-loader?name=fonts/[name]-[hash:8].[ext]' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=fonts/[name]-[hash:8].[ext]' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=fonts/[name]-[hash:8].[ext]' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=img/[name]-[hash:8].[ext]' },
       // Bootstrap 3
-      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+      { test: /bootstrap-sass\/assets\/javascripts\//,
+        loader: 'imports?jQuery=jquery' },
       // Bootstrap 4
-      { test: /bootstrap\/build\/js\/umd\//, loader: 'imports?jQuery=jquery' }
+      { test: /bootstrap\/build\/js\/umd\//,
+        loader: 'imports?jQuery=jquery' }
     ]
   },
-  plugins: plugins
+  plugins: plugins,
+  performance: { hints: production ? "warning" : false }
 };
 
 module.exports = config;
