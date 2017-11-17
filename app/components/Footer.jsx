9707919bfa3fa8f0f98e99f34329e7296dc5ff0a
Rafael Villar Burke <pachi@ietcc.csic.es>
2017-11-17 12:39:50 +0100

    Footer: Lleva estilo a css

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = _props => (
-  <div className="navbar navbar-bottom text-center" style={{ marginTop: 20 + 'px' }}>
+  <div className="navbar navbar-bottom text-center footer">
     <p className="text-muted small center">VisorEPBD - 2017 - <a href="http://www.fomento.gob.es">Ministerio de Fomento</a>, <a href="http://www.ietcc.csic.es">Instituto Eduardo Torroja de Ciencias de la Construcción (IETcc-CSIC)</a>.</p>
   </div>);
 
