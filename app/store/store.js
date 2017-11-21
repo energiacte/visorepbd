67be912b5e22cfd269e82567a0e9859c9ba89534
Rafael Villar Burke <pachi@ietcc.csic.es>
2017-11-21 12:05:29 +0100

    store: usa ejemplo de cte_test_carriers para estado inicial

--- app/store/store.js
+++ app/store/store.js
@@ -7,53 +7,118 @@ const initialState = {
   data: { ren: 0, nren: 0, total: 0, rer: 0 },
   storedcomponent: {
     active: true,
-    type: 'CARRIER',
-    carrier: 'ELECTRICIDAD',
-    ctype: 'CONSUMO',
-    csubtype: 'EPB',
-    values: [9.67, 7.74, 4.84, 4.35, 2.42, 2.90, 3.87, 3.39, 2.42, 3.87, 5.80, 7.74]
+    carrier: "ELECTRICIDAD",
+    ctype: "PRODUCCION",
+    csubtype: "INSITU",
+    service: "NDEF",
+    values: [ 34.21, 41.94, 64.94, 73.88, 88.44, 88.64, 91.04, 76.15, 52.84, 39.26, 27.43, 26.26 ],
+    comment: "PV 5m2 (5kWp)"
   },
   selectedkey: 0, // Currently selected component
   kexp: 0.0,
-  area: 1,
+  area: 200,
   localizacion: 'PENINSULA',
   wfactors: cte.new_wfactors('PENINSULA'),
   components: [ // Energy components array (ejemplo3PVBdC.csv)
     {
       active: true,
-      type: 'CARRIER',
-      carrier: 'ELECTRICIDAD',
-      ctype: 'CONSUMO',
-      csubtype: 'EPB',
-      values: [9.67, 7.74, 4.84, 4.35, 2.42, 2.90, 3.87, 3.39, 2.42, 3.87, 5.80, 7.74],
-      comment: 'Linea 1 de ejemplo3PVBdC.csv'
+      carrier: "ELECTRICIDAD",
+      ctype: "PRODUCCION",
+      csubtype: "INSITU",
+      service: "NDEF",
+      values: [ 34.21, 41.94, 64.94, 73.88, 88.44, 88.64, 91.04, 76.15, 52.84, 39.26, 27.43, 26.26 ],
+      comment: "PV 5m2 (5kWp)"
     },
     {
       active: true,
-      type: 'CARRIER',
-      carrier: 'ELECTRICIDAD',
-      ctype: 'PRODUCCION',
-      csubtype: 'INSITU',
-      values: [1.13, 1.42, 1.99, 2.84, 4.82, 5.39, 5.67, 5.11, 4.54, 3.40, 2.27, 1.42],
-      comment: 'Linea 2 de ejemplo3PVBdC.csv'
+      carrier: "MEDIOAMBIENTE",
+      ctype: "PRODUCCION",
+      csubtype: "INSITU",
+      service: "ACS",
+      values: [ 41.05, 50.33, 77.92, 88.66, 106.13, 106.36, 109.25, 91.38, 63.4, 47.11, 32.92, 31.51 ],
+      comment: "PST 2m2, n=0.30"
     },
     {
       active: true,
-      type: 'CARRIER',
-      carrier: 'MEDIOAMBIENTE',
-      ctype: 'CONSUMO',
-      csubtype: 'EPB',
-      values: [21.48, 17.18, 10.74, 9.66, 5.37, 6.44, 8.59, 7.52, 5.37, 8.59, 12.89, 17.18],
-      comment: 'Linea 3 de ejemplo3PVBdC.csv'
+      carrier: "MEDIOAMBIENTE",
+      ctype: "CONSUMO",
+      csubtype: "EPB",
+      service: "ACS",
+      values: [ 41.05, 50.33, 77.92, 88.66, 106.13, 106.36, 109.25, 91.38, 63.4, 47.11, 32.92, 31.51 ],
+      comment: "PST 2m2, n=0.30"
     },
     {
       active: true,
-      type: 'CARRIER',
-      carrier: 'MEDIOAMBIENTE',
-      ctype: 'PRODUCCION',
-      csubtype: 'INSITU',
-      values: [21.48, 17.18, 10.74, 9.66, 5.37, 6.44, 8.59, 7.52, 5.37, 8.59, 12.89, 17.18],
-      comment: 'Linea 4 de ejemplo3PVBdC.csv'
+      carrier: "ELECTRICIDAD",
+      ctype: "CONSUMO",
+      csubtype: "EPB",
+      service: "ACS",
+      values: [ 53.94, 49.74, 47.57, 42.69, 24.37, 34.64, 33.33, 31.08, 54.17, 61.58, 57.66, 68.67 ],
+      comment: "BdC ind. aire-agua n_gen=2.5 n_d+e+c=0.88"
+    },
+    {
+      active: true,
+      carrier: "MEDIOAMBIENTE",
+      ctype: "CONSUMO",
+      csubtype: "EPB",
+      service: "ACS",
+      values: [ 80.92, 74.61, 71.36, 64.04, 36.56, 51.97, 50, 46.62, 81.26, 92.36, 86.48, 103 ],
+      comment: "BdC ind. aire-agua n_gen=2.5 n_d+e+c=0.88"
+    },
+    {
+      active: true,
+      carrier: "ELECTRICIDAD",
+      ctype: "CONSUMO",
+      csubtype: "EPB",
+      service: "CAL",
+      values: [ 269.05, 167.46, 87.63, 18.85, 12.93, 0, 0, 0, 0, 6.19, 60.04, 240.41 ],
+      comment: "BdC ind. aire-agua n_gen=3.0 n_d+e+c=0.95"
+    },
+    {
+      active: true,
+      carrier: "MEDIOAMBIENTE",
+      ctype: "CONSUMO",
+      csubtype: "EPB",
+      service: "CAL",
+      values: [ 538.1, 334.93, 175.26, 37.69, 25.85, 0, 0, 0, 0, 12.37, 120.08, 480.83 ],
+      comment: "BdC ind. aire-agua n_gen=3.0 n_d+e+c=0.95"
+    },
+    {
+      active: true,
+      carrier: "ELECTRICIDAD",
+      ctype: "CONSUMO",
+      csubtype: "EPB",
+      service: "REF",
+      values: [ 0, 0, 0, 0, 0, 2.04, 23.34, 13.02, 17.72, 0, 0, 0 ],
+      comment: "BdC ind. aire-agua n_gen=2.5 n_d+e+c=0.95"
+    },
+    {
+      active: true,
+      carrier: "ELECTRICIDAD",
+      ctype: "CONSUMO",
+      csubtype: "EPB",
+      service: "VEN",
+      values: [ 98.74, 89.18, 98.74, 95.55, 98.74, 95.55, 98.74, 98.74, 95.55, 98.74, 95.55, 98.74 ],
+      comment: "Extractor simple flujo"
+    },
+    {
+      active: true,
+      carrier: "MEDIOAMBIENTE",
+      ctype: "PRODUCCION",
+      csubtype: "INSITU",
+      service: "ACS",
+      values: [ 80.92, 74.61, 71.36, 64.04, 36.56, 51.97, 50, 46.62, 81.26, 92.36, 86.48, 103.0 ],
+      comment: "Equilibrado de energía térmica insitu (MEDIOAMBIENTE) consumida y sin producción declarada"
+    },
+    {
+      active: true,
+      carrier: "MEDIOAMBIENTE",
+      ctype: "PRODUCCION",
+      csubtype: "INSITU",
+      service: "CAL",
+      values: [
+538.1, 334.93, 175.26, 37.69, 25.85, 0, 0, 0, 0, 12.37, 120.08, 480.83         ],
+      comment: "Equilibrado de energía térmica insitu (MEDIOAMBIENTE) consumida y sin producción declarada"
     }
   ],
   currentfilename: 'csvEPBDpanel.csv'
