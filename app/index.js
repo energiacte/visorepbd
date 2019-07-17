0764f7a5034689b0692d0375204070c9bf1c83c4
Rafael Villar Burke <pachi@ietcc.csic.es>
2019-07-17 13:58:53 +0200

    Carga dinámicamente el módulo principal para poder usar wasm

--- app/index.js
+++ app/index.js
@@ -1,43 +1,6 @@
-// Recursos generales
-import "img/favicon.ico";
-// Polyfills
-import "@babel/polyfill/noConflict";
-// Bootstrap
-import "jquery";
-import "popper.js";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "styles/font-awesome.min.css"; // copia de iconos fontawesome
-
-// Import React and JS
-import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-
-// Elementos propios
-import "styles/style.scss"; // CSS
-import store from "store/store.js";
-import MainPage from "components/MainPage.jsx";
-import WeightingFactorsPage from "components/WeightingFactorsPage.jsx";
-import HelpPage from "components/HelpPage.jsx";
-import AboutPage from "components/AboutPage.jsx";
-
-// Render
-ReactDOM.render(
-  <Provider store={store}>
-    <HashRouter>
-      <Switch>
-        <Route
-          exact
-          path="/weightingfactors"
-          component={WeightingFactorsPage}
-        />
-        <Route exact path="/help" component={HelpPage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/" component={MainPage} />
-      </Switch>
-    </HashRouter>
-  </Provider>,
-  document.body.appendChild(document.createElement("div"))
-);
+// A dependency graph that contains any wasm must all be imported
+// asynchronously. This `index.js` file does the single async import, so
+// that no one else needs to worry about it again.
+import("./main.js")
+  // eslint-disable-next-line no-console
+  .catch(e => console.error("Error importing `index.js`:", e));
---
+++ app/main.js
@@ -0,0 +1,43 @@
+// Recursos generales
+import "img/favicon.ico";
+// Polyfills
+import "@babel/polyfill/noConflict";
+// Bootstrap
+import "jquery";
+import "popper.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "styles/font-awesome.min.css"; // copia de iconos fontawesome
+
+// Import React and JS
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+// Elementos propios
+import "styles/style.scss"; // CSS
+import store from "store/store.js";
+import MainPage from "components/MainPage.jsx";
+import WeightingFactorsPage from "components/WeightingFactorsPage.jsx";
+import HelpPage from "components/HelpPage.jsx";
+import AboutPage from "components/AboutPage.jsx";
+
+// Render
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        <Route
+          exact
+          path="/weightingfactors"
+          component={WeightingFactorsPage}
+        />
+        <Route exact path="/help" component={HelpPage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/" component={MainPage} />
+      </Switch>
+    </HashRouter>
+  </Provider>,
+  document.body.appendChild(document.createElement("div"))
+);
