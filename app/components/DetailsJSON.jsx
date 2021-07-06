47cd1f7c5cc21fa67901d9add3f2f6aca4f69ba7
Rafael Villar Burke <pachi@ietcc.csic.es>
2021-07-06 18:37:03 +0200

    Corrige uso de balance para acs_nrb que ya no se usa

--- app/components/DetailsJSON.jsx
+++ app/components/DetailsJSON.jsx
@@ -4,17 +4,13 @@ import React from "react";
 const DetailsJSON = props => {
   const { balance } = props;
 
-  if (!balance.ep || !balance.ep_acs_nrb) return null;
+  if (balance.ep === undefined) return null;
 
   return (
     <div className="row">
       <div className="col-lg-6">
         <h3>Energía primaria y emisiones:</h3>
         <pre>{JSON.stringify(balance.ep, undefined, 2)}</pre>
-      </div>
-      <div className="col-lg-6">
-        <h3>Energía primaria y emisiones para ACS en perímetro próximo:</h3>
-        <pre>{JSON.stringify(balance.ep_acs_nrb, undefined, 2)}</pre>
       </div>
     </div>
   );
